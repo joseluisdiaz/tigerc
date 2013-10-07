7b6ff33070af6b228714145538ad0f896378408c
Jose Luis Diaz <jdiaz@socialmetrix.com>
2013-10-07 15:03:47 -0300

    Topological sort support

---
+++ src/main/scala/tiger/Util.scala
@@ -0,0 +1,29 @@
+package tiger
+
+import scala.annotation.tailrec
+
+/**
+ * User: jose
+ * Date: 10/5/13
+ * Time: 9:21 PM
+ */
+object Util {
+  def tsort[A](edges: Traversable[(A, A)]): Iterable[A] = {
+
+    @tailrec
+    def tsort(toPreds: Map[A, Set[A]], done: Iterable[A]): Iterable[A] = {
+      val (noPreds, hasPreds) = toPreds.partition { _._2.isEmpty }
+      if (noPreds.isEmpty) {
+        if (hasPreds.isEmpty) done else sys.error(hasPreds.toString)
+      } else {
+        val found = noPreds.map { _._1 }
+        tsort(hasPreds.mapValues { _ -- found }, done ++ found)
+      }
+    }
+
+    val toPred = edges.foldLeft(Map[A, Set[A]]()) { (acc, e) =>
+      acc + (e._1 -> acc.getOrElse(e._1, Set())) + (e._2 -> (acc.getOrElse(e._2, Set()) + e._1))
+    }
+    tsort(toPred, Seq())
+  }
+}
